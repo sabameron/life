9074362261222815aa177885202f18fc8a344bed
sabameron <122842741+sabameron@users.noreply.github.com>
2024-12-12 15:10:28 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+default = {
+    "サービス展開" : 24,
+    "権利販売" : 30,
+    "財団設立" : 40,
+    "資金" : 200000
+}
+
+def main():
+  print(f"サービス展開: {default['サービス展開']}")
+  print(f"権利販売: {default['権利販売']}")
+  print(f"財団設立: {default['財団設立']}")
+  print(f"資金: {default['資金']}")
+
+if __name__ == "__main__":
+    main()
